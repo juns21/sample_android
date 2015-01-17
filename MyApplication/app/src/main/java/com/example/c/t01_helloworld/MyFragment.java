7a95e806e1e8dd5c33c3a33f1acd8a6d343a6a23
sj cho <juns0201@gmail.com>
2015-01-17 14:43:02 +0900

    hello commit2

---
+++ MyApplication/app/src/main/java/com/example/c/t01_helloworld/MyFragment.java
@@ -0,0 +1,34 @@
+package com.example.c.t01_helloworld;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MyFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_my, container, false);
+        View v = inflater.inflate(R.layout.fragment_my, container, false);
+
+        Button myButton =  (Button)v.findViewById(R.id.myButton);
+        final TextView myTextView = (TextView) v.findViewById(R.id.textView);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int count;
+                count = Integer.parseInt(myTextView.getText().toString());
+                count++;
+                myTextView.setText(""+count);
+            }
+        });
+
+        return v;
+    }
+}
