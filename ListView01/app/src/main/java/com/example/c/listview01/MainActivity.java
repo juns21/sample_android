c9b97c73fcdb2165610e9aafa572a6123dabb845
sj cho <juns0201@gmail.com>
2015-01-18 10:36:00 +0900

    basic list view

--- ListView01/app/src/main/java/com/example/c/listview01/MainActivity.java
+++ ListView01/app/src/main/java/com/example/c/listview01/MainActivity.java
@@ -1,17 +1,41 @@
 package com.example.c.listview01;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+    String[] list = {"Hello", "World", "Oracle", "Java",
+            "Hello", "World", "Oracle", "Java",
+            "Hello", "World", "Oracle", "Java",
+            "Hello", "World", "Oracle", "Java"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView)findViewById(R.id.listView);
+
+
+        ArrayAdapter<String> adapter;
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //((TextView)view).getText().toString();
+                Toast.makeText(MainActivity.this, list[position], Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 
