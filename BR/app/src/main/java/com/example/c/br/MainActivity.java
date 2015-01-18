91b1247511d2a3bdc5e820465d3c0ad137876492
sj cho <juns0201@gmail.com>
2015-01-18 17:52:56 +0900

    self broadcast receiver

--- BR/app/src/main/java/com/example/c/br/MainActivity.java
+++ BR/app/src/main/java/com/example/c/br/MainActivity.java
@@ -1,31 +1,49 @@
 package com.example.c.br;
 
+import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
     MyBR br;
+    MyBR_noti brNoti;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.c.br");
+                sendBroadcast(intent);
+            }
+        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        br = new MyBR();
+        brNoti = new MyBR_noti();
+        String action = "com.example.c.br";
+        IntentFilter filter = new IntentFilter(action);
+        registerReceiver(brNoti, filter);
+
+        /*br = new MyBR();
         String action = "android.provider.Telephony.SMS_RECEIVED";
         //registerReceiver(br, android.provider.Telephony.SMS_RECEIVED);
 
         IntentFilter filter = new IntentFilter(action);
-        registerReceiver(br, filter);
+        registerReceiver(br, filter);*/
 
     }
 
@@ -33,7 +51,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onStop() {
         super.onStop();
 
-        unregisterReceiver(br);
+        unregisterReceiver(brNoti);
+
+        /*unregisterReceiver(br);*/
     }
 
     @Override
