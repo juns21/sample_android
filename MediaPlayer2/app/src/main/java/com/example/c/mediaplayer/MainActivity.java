47555c8a5ac0858ab900d385c228e8b5920f7a2f
sj cho <juns0201@gmail.com>
2015-01-25 16:11:09 +0900

    mediaplayer

--- MediaPlayer2/app/src/main/java/com/example/c/mediaplayer/MainActivity.java
+++ MediaPlayer2/app/src/main/java/com/example/c/mediaplayer/MainActivity.java
@@ -1,19 +1,101 @@
 package com.example.c.mediaplayer;
 
+import android.media.MediaPlayer;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
+    MediaPlayer mp = new MediaPlayer();
+    boolean isPlaying = false;
+    String path;
+    SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        path = Environment.getExternalStorageDirectory().toString();
+        path += "/Samsung/Music/Over_the_horizon.mp3";
+
+        Button btnStart = (Button) findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //mp = new MediaPlayer();
+                try {
+                    mp.setDataSource(path);
+                    mp.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                seekBar.setMax(mp.getDuration());
+                mp.start();
+            }
+        });
+
+        Button btnStop = (Button) findViewById(R.id.btnStop);
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mp != null) {
+                    mp.stop();
+                    mp.release();
+                    mp = null;
+                    isPlaying = false;
+                }
+            }
+        });
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mp.seekTo(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                mp.pause();
+                isPlaying = false;
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mp.seekTo(seekBar.getProgress());
+                mp.start();
+                isPlaying = true;
+            }
+        });
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (isPlaying) {
+                        if (mp != null) {
+                            if (mp.isPlaying()) {
+                                seekBar.setProgress(mp.getCurrentPosition());
+                            }
+                        }
+                    }
+                }catch (Exception e) {
+                    seekBar.setProgress(0);
+                }
+
+            }
+        }).start();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
