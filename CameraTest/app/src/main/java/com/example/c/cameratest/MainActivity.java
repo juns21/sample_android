7ac3f5cb557a08cffd83cb80e988c1b0eb19eb76
sj cho <juns0201@gmail.com>
2015-02-01 10:49:28 +0900

    camera preview layout

--- CameraTest/app/src/main/java/com/example/c/cameratest/MainActivity.java
+++ CameraTest/app/src/main/java/com/example/c/cameratest/MainActivity.java
@@ -3,14 +3,22 @@ package com.example.c.cameratest;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 
 public class MainActivity extends ActionBarActivity {
     private Camera mCamera;
+    private CameraPreview mPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +37,37 @@ public class MainActivity extends ActionBarActivity {
         }else {
             Toast.makeText(this, "카메라가 없군요~", Toast.LENGTH_LONG).show();
         }
+
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+
+        Button captureButton = (Button) findViewById(R.id.button_capture);
+        captureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCamera.takePicture(null, null, mPic);
+            }
+        });
     }
+
+    private Camera.PictureCallback mPic = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            String path = Environment.getExternalStorageDirectory().toString();
+            path += "/temp/cameraTest.jpg";
+
+            File file = new File(path);
+            try {
+                FileOutputStream fos = new FileOutputStream(file);
+                fos.write(data);
+                fos.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    };
 
 
     @Override
